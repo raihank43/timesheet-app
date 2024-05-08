c11dda8b4a150da5278a011798a3a1c92d36a5ea
raihank43 <raihankusuma5@gmail.com>
2024-05-08 19:36:14 +0700

    feat: getEmployeeActivities

--- backend/controllers/activity.js
+++ backend/controllers/activity.js
@@ -1,9 +1,22 @@
-const { Activity } = require("../models");
+const { Activity, Project } = require("../models");
 
 module.exports = class ActivityController {
   static async getAllActivities(req, res) {
     try {
       const activities = await Activity.findAll();
+      res.status(200).json(activities);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  static async getEmployeeActivities(req, res) {
+    try {
+      const { EmployeeId } = req.params;
+      const activities = await Activity.findAll({
+        include: [{ model: Project }],
+        where: { EmployeeId },
+      });
       res.status(200).json(activities);
     } catch (error) {
       res.status(500).json({ message: error.message });
--- backend/routes/activity.js
+++ backend/routes/activity.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ActivityController = require("../controllers/activity");
 
 router.get("/", ActivityController.getAllActivities); // This is the route that the frontend will call to get all activities
+router.get("/:EmployeeId", ActivityController.getEmployeeActivities); // This is the route that the frontend will call to get all activities for a specific employee
 
 module.exports = router;
