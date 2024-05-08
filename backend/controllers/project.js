40e4f1898b31cce34662321d377f973e7d1b4b74
raihank43 <raihankusuma5@gmail.com>
2024-05-08 17:53:02 +0700

    feat: add projects

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -9,4 +9,15 @@ module.exports = class ProjectController {
       res.status(500).json({ message: error.message });
     }
   }
+  static async addProject(req, res) {
+    try {
+      const { name } = req.body;
+      const project = await Project.create({ name });
+      res.status(201).json(project);
+    } catch (error) {
+      error.name = "SequelizeValidationError"
+        ? res.status(400).json({ message: error.errors[0].message })
+        : res.status(400).json({ message: error.message });
+    }
+  }
 };
--- backend/routes/project.js
+++ backend/routes/project.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ProjectController = require("../controllers/project");
 
 router.get("/", ProjectController.getAllProjects); // This is the route that the frontend will call to get all projects
+router.post("/", ProjectController.addProject); // This is the route that the frontend will call to add a project
 
 module.exports = router;
