8b483a650d7cf02f91f000e2238c2dada2634b37
raihank43 <raihankusuma5@gmail.com>
2024-05-08 17:43:29 +0700

    feat: add projects table, migration, and seeding files

---
+++ backend/models/project.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Project extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Project.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Name is required",
+          },
+          notEmpty: {
+            msg: "Name cannot be empty",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Project",
+    }
+  );
+  return Project;
+};
