38a2ff3a7b18f39025837167e32a982aed1c352d
raihank43 <raihankusuma5@gmail.com>
2024-05-08 07:31:36 +0700

    feat: setting up deployment

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
-require("dotenv").config(); // Load .env file
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config(); // Load .env file
+}
+
 const express = require("express");
-const { json } = require("sequelize");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes"));
-
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
