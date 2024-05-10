34723a2ade8ef3eecb611b4eff4f995728de771d
raihank43 <raihankusuma5@gmail.com>
2024-05-10 19:28:25 +0700

    feat: convertMinutesToHours

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,6 +4,7 @@ import { Button, Table } from "@mui/joy";
 import TableComponent from "./components/TableComponent";
 import ButtonComponent from "./components/ButtonComponent";
 import EmployeeDetail from "./components/EmployeeDetail";
+import convertMinutesToHours from "./utils/convertMinuteToHours";
 
 async function fetchEmployees() {
   const response = await fetch(`${baseURL}employees`);
@@ -17,12 +18,36 @@ async function fetchEmployee(id) {
   return employee;
 }
 
+async function fetchActivities(id) {
+  const response = await fetch(`${baseURL}activities/${id}`);
+  const activities = await response.json();
+  return activities;
+}
+
 export default async function Home() {
   const employees = await fetchEmployees();
+  const employee = await fetchEmployee(1);
+  console.log(employee);
   return (
     <main className=" min-h-screen m-6 bg-white p-6 rounded-lg  shadow-2xl">
       <EmployeeDetail employees={employees} />
       <TableComponent />
+
+      <div className="flex justify-between mt-4 p-6">
+        <div>
+          <h1 className="text-custom-blue font-bold text-lg">Total Durasi</h1>
+          <h1 className="text-custom-blue font-black text-xl">
+            Total Pendapatan
+          </h1>
+        </div>
+
+        <div className="flex flex-col  justify-end items-end">
+          <h1 className="text-custom-blue font-bold text-lg">
+            {convertMinutesToHours(185)}
+          </h1>
+          <h1 className="text-custom-blue font-black text-xl">{180000}</h1>
+        </div>
+      </div>
     </main>
   );
 }
