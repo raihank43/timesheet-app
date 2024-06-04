7fa54395b4ef28a4fc2131a73d383dc3bf981334
raihank43 <raihankusuma5@gmail.com>
2024-06-04 15:36:15 +0700

    feat: update logo and header

--- frontend/src/app/components/HeaderComponent.js
+++ frontend/src/app/components/HeaderComponent.js
@@ -26,7 +26,7 @@ export default function HeaderComponent() {
         } transition-transform duration-500 flex items-center justify-between p-4 bg-white w-full shadow-lg z-10`}
       >
         <div className="flex items-center gap-2">
-          <img src="/HHLogo.png" alt="Logo" className="h-10" />
+          <img src="/timesheet-logo.png" alt="Logo" className="h-10" />
           <div className="flex flex-col items-center ">
             <h1 className="text-lg font-black  text-custom-red">Timesheet</h1>
             <h1 className="text-lg font-black text-custom-red">Management</h1>
@@ -34,7 +34,17 @@ export default function HeaderComponent() {
         </div>
       </header>
       <div className="w-full flex ">
-        <h1 className="p-6 text-2xl font-bold text-black">HH Timesheet</h1>
+        <div className="flex  justify-center items-center p-6 gap-3">
+          <img src="/timesheet-logo.png" alt="Logo" className="h-10" />
+          <div className="flex justify-evenly flex-col ">
+            <h1 className="text-2xl font-bold text-custom-red ">
+              Timesheet Management App
+            </h1>
+            <p className="text-custom-blue text-sm">
+              A tool designed to track freelancer activities.
+            </p>
+          </div>
+        </div>
       </div>
       <nav className="flex items-center justify-between p-2 bg- w-full shadow-lg">
         <TabsComponent />
